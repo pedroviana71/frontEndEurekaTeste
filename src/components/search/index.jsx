8f7ad5f0bf502608e76a552f8d7667644ba93904
pedroviana71 <pedrohenrique.viana5@gmail.com>
2022-10-07 15:45:32 -0300

    implemented api requisition and minor css adjusts

--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -1,6 +1,37 @@
+import { useState } from "react";
 import styles from "./index.module.css";
+import { api } from "../../actions";
+
 const Search = () => {
-  return <div className={styles.searchContainer}>Search</div>;
+  const [cep, setCep] = useState("");
+  const [address, setAddress] = useState({});
+
+  const handleSearch = async () => {
+    const data = await api.get(`http://localhost:3001/?cep=${cep}`);
+    setAddress(data.data);
+    console.log(data.data);
+  };
+
+  const handleChangeCEP = (e) => {
+    setCep(e.target.value);
+  };
+
+  return (
+    <div className={styles.searchContainer}>
+      <input
+        type="number"
+        placeholder="Digite o CEP"
+        onChange={handleChangeCEP}
+      />
+      <button onClick={() => handleSearch(cep)}>Pesquisar</button>
+      {address.logradouro && (
+        <div>
+          <p>Bairro: {address.bairro}</p>
+          <p>Logradouro: {address.logradouro}</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Search;
